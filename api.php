0ec7af86727f0c36550871fa75a573d460fcb9d2
dateolive <2448282543@qq.com>
2020-08-22 19:06:51 +0800

    feat(Controller):新拟态个人仓库页面

---
+++ api.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    // 缓存github数据
+    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; GreenBrowser)');
+    $api_url ='https://api.github.com/users/dateolive/repos';  // 拼合github
+    if (!file_exists("github.json")) {  // 缓存github API数据24小时
+        file_put_contents("github.json", file_get_contents($api_url));
+    } else if ((time() - filemtime("github.json")) > 86400) {
+        file_put_contents("github.json", file_get_contents($api_url));
+    }
+   
+?>
+
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>github</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+<div class="container">
+<?php
+    $json = file_get_contents('github.json');
+    $data=json_decode($json,true);
+    $length=sizeof($data);   
+    for($i=0;$i<$length;$i++)
+    {
+?>
+  <div class="card">
+    <div class="imgBx">
+      <img src="./github.svg" alt="">
+    </div>
+    <div class="contentBx">
+      <h2><?php echo $data[$i]['name'] ?></h2>
+      <h3 class="new-item-badge"><?php 
+      if($data[$i]['language']==NULL)
+      echo "NULL"; 
+      else echo  $data[$i]['language'];
+      ?></h3>
+      <h4><?php echo $data[$i]['stargazers_count'] ?> stars / <?php echo $data[$i]['forks'] ?> forks</h4>
+      <p><?php 
+      if($data[$i]['description']==NULL)
+      echo "欢迎来到github个人仓库页面，各位大佬给个star吧"; 
+      else echo  $data[$i]['description'];
+      ?></p>
+      <a href="<?php echo $data[$i]['html_url'] ?>"><span>Read More</span></a>
+    </div> 
+  </div>
+  <?php } ?>
+</div>
+
+    </body>
+</html>
